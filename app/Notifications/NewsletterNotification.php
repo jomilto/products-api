d91ec3b64aa5bcdec63cc3a2a4dab62a13482a8d
JLob Lap <joselobatof@gmail.com>
2020-09-19 22:25:00 -0500

    Command schedule

--- app/Notifications/NewsletterNotification.php
+++ app/Notifications/NewsletterNotification.php
@@ -2,8 +2,8 @@
 
 namespace App\Notifications;
 
+use App\Product;
 use Illuminate\Bus\Queueable;
-use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
 
@@ -12,19 +12,19 @@ class NewsletterNotification extends Notification
     use Queueable;
 
     /**
-     * Create a new notification instance.
-     *
-     * @return void
+     * @var Product[]
      */
-    public function __construct()
+    private $products;
+
+    public function __construct(array $products)
     {
-        //
+        $this->products = $products;
     }
 
     /**
      * Get the notification's delivery channels.
      *
-     * @param  mixed  $notifiable
+     * @param mixed $notifiable
      * @return array
      */
     public function via($notifiable)
@@ -35,27 +35,21 @@ class NewsletterNotification extends Notification
     /**
      * Get the mail representation of the notification.
      *
-     * @param  mixed  $notifiable
+     * @param mixed $notifiable
      * @return \Illuminate\Notifications\Messages\MailMessage
      */
     public function toMail($notifiable)
     {
-        return (new MailMessage)
-                    ->line('The introduction to the notification.')
-                    ->action('Notification Action', url('/'))
-                    ->line('Thank you for using our application!');
+        $mailMessage = new MailMessage;
+        $mailMessage->line('Estos son los productos con mas rating en la ultima semana');
+
+        foreach ($this->products as $product) {
+            $mailMessage->line($product['name']);
+        }
+
+        $mailMessage->line('Thank you for using our application!');
+
+        return $mailMessage;
     }
 
-    /**
-     * Get the array representation of the notification.
-     *
-     * @param  mixed  $notifiable
-     * @return array
-     */
-    public function toArray($notifiable)
-    {
-        return [
-            //
-        ];
-    }
 }
